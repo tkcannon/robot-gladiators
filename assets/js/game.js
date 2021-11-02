1ac29d5267529012856321f82beaa8467da84420
tkcannon <taylorkcannon@gmail.com>
2021-11-02 17:48:40 -0600

    added multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,20 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
     var promtFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 
     // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
+    if (promtFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
@@ -62,4 +59,7 @@ var fight = function () {
         window.alert("You need to choose a valid option. Try again!");
     }
 }
-fight();
+
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
