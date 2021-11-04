60d43eb329f8916d6710d3660270da5cbf57c198
tkcannon <taylorkcannon@gmail.com>
2021-11-04 16:40:09 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -84,6 +84,11 @@ var startGame = function () {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+            if (i < enemyNames.length - 1 && playerHealth > 0) {
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                shop();
+            }
         }
         else {
             window.alert("You have lost your robot in battle! Game over");
@@ -107,6 +112,38 @@ var endGame = function () {
     }
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+}
+
+var shop = function () {
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    switch (shopOptionPrompt) {
+        case "refill":
+            if (playerMoney >= 7) {
+              window.alert("Refilling player's health by 20 for 7 dollars.");
+          
+              // increase health and decrease money
+              playerHealth = playerHealth + 20;
+              playerMoney = playerMoney - 7;
+            }
+            else {
+              window.alert("You don't have enough money!");
+            }
+          
+            break;
+          case "upgrade":
+            if (playerMoney >= 7) {
+              window.alert("Upgrading player's attack by 6 for 7 dollars.");
+          
+             // increase attack and decrease money
+              playerAttack = playerAttack + 6;
+              playerMoney = playerMoney - 7;
+            }
+            else {
+              window.alert("You don't have enough money!");
+            }
+          
+            break;
     }
 }
 
